f42b5c8adaa4b1cfe2cca3a044ce9e5f7747a0b1
extusharkawade~ <tushar.kawade@excellarate.com~>
2024-01-28 14:44:29 +0530

    updated tech stack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,36 +8,39 @@ import {
   DiPython,
   DiGit,
   DiJava,
+  DiAndroid,
+  DiMysql,
 } from "react-icons/di";
+import { SiSpringboot } from "react-icons/si";
+
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpringboot />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiAndroid />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      
-      <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
-      
-      
     </Row>
   );
 }
